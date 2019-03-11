8eef65eb0d77446867ffa46bcb399406fc9359f2
Krasnoshtandenis <krasnoshtan.job4j@yandex.ru>
2019-03-11 10:40:12 +0300

    1.3. JavaDoc. Code convention[#113600]

--- chapter_001/src/main/java/ru/Krasnoshtandenis/Calculate.java
+++ chapter_001/src/main/java/ru/Krasnoshtandenis/Calculate.java
@@ -1,6 +1,17 @@
 package ru.Krasnoshtandenis;
 
+/**
+* Class Calculate, a string console output.
+* @author Denis Krasnoshtan (krasnoshtan.job4j@yandex.ru)
+* @version 1.0
+* @since 11.03.2019
+*/
+
 public class Calculate {
+	    /** 
+		* Method main, output the string to the console.
+		* @param args - args.
+		*/
        public String echo(String value) {
              return String.format("%s %s %s", value, value, value);
          }
